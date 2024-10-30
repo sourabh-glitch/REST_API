0f828d479590d5225116360d0dba27f4c61b2b48
sourabh-glitch <upadhaysourav14@gmail.com>
2024-10-30 20:13:22 +0530

    new_commit

--- index.js
+++ index.js
@@ -8,12 +8,36 @@ app.use(express.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.set("views" , path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 
-app.get("/", (req, res)=>{
-    res.send("serving working well")
+let posts= [
+    {
+        username:"Apncollege",
+        content: "Code is the key"
+    },
+    {
+        username:"sourabh",
+        content: "Code is the key"
+    },
+    {
+        username:"gourav",
+        content: "Code is the key"
+    },
+]
+
+app.get("/posts", (req, res)=>{
+    res.render("index.ejs" , {posts})
 });
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs")
+}); 
+app.post("/posts", (req, res) => {
+    let {username, content} = req.body;
+    posts.push({username, content});
+    res.send("post reqyuest wroking ")
+}); 
 
 app.listen(port, () =>{
     console.log("listeing to port : 8080")
