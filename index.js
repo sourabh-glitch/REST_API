639b340ee408c291ed802094bd6adf8c3b43c321
sourabh-glitch <upadhaysourav14@gmail.com>
2024-11-06 01:13:14 +0530

    final_Commit

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const port = 8080;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs")
 app.set("views" , path.join(__dirname, "views"));
@@ -38,6 +40,7 @@ app.get("/posts", (req, res)=>{
 app.get("/posts/new", (req, res) => {
     res.render("new.ejs")
 }); 
+
 app.post("/posts", (req, res) => {
     let {username, content} = req.body;
     let id = uuidv4();
@@ -46,12 +49,37 @@ app.post("/posts", (req, res) => {
     res.redirect("/posts")
 }); 
 
-app.get("/posts/:id", (req, res) => {
+app.get("/posts/:id", (req, res) =>{
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", {post})
+
+
+})
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newcontent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newcontent;
+    console.log(post);
+    res.redirect("/posts")
+}); 
+
+app.get("/posts/:id/edit", (req, res) =>{
     let { id } = req.params;
     let post = posts.find((p) => id === p.id)
-    console.log(post);
-    res.render("show.ejs", { post })
-}); 
+    console.log(post)
+    res.render("edit.ejs" , { post })
+
+} );
+
+app.delete("/posts/:id", (req,res) =>{
+
+    let { id } = req.params;
+     posts = posts.filter((p) => id !== p.id)
+    res.redirect("/posts")
+})
 
 app.listen(port, () =>{
     console.log("listeing to port : 8080")
